444e89c422139729b4ae686dae40f38fd97acbde
Simon Dorrer <simon.dorrer@jku.at>
2025-11-02 13:45:47 +0100

    renamed project.v to tt_um_TinyWhisper.v

--- src/tt_um_TinyWhisper.v
+++ src/tt_um_TinyWhisper.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2025 Simon Dorrer, Jonathan Hager, Matthias Jung, Harald Pretl
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_TinyWhisper (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
@@ -19,5 +19,9 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+// ToDo Jonathan & Simon: Include top-level module (TinyWhisper)
+
+// Pin Assignments (ToDo)
 
 endmodule
